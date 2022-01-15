824aae7a9edb54b0cf2e16e5154e2524238c4673
NPavanReddy <pavanreeddy.nalla123@gmail.com>
2022-01-15 18:50:32 +0530

    new

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function MeetupDetails() {
+  return (
+    <Fragment>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg" />
+      <h1>A First Meetup</h1>
+      <address>Some stree5, some city ,123456</address>
+      <p>The meetup description</p>
+    </Fragment>
+  );
+}
+
+export default MeetupDetails;
